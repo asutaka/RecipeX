6e0604a2eb4c125e9a41f0a72444e0f1d5be6405
phunv@ftech.ai <phunv@ftech.ai>
2023-11-23 23:16:35 +0700

    add api cook

--- recipe_x/lib/data/network/constants/endpoints.dart
+++ recipe_x/lib/data/network/constants/endpoints.dart
@@ -5,6 +5,8 @@ class Endpoints {
   static const String baseAddress =
       "https://654ca2f077200d6ba859225c.mockapi.io";
   static const String baseUrl = "http://jsonplaceholder.typicode.com";
+  static const String baseAddress2 =
+      "https://655f7611879575426b4563ca.mockapi.io";
 
   // receiveTimeout
   static const int receiveTimeout = 15000;
@@ -19,4 +21,8 @@ class Endpoints {
   static const String api_video = "$baseAddress/api_video";
   //resource
   static const String api_resource = "$baseAddress/api_resource";
+  //new cook
+  static const String api_newcook = "$baseAddress/api_newcook";
+  //fire cook
+  static const String api_firecook = "$baseAddress/api_firecook";
 }
